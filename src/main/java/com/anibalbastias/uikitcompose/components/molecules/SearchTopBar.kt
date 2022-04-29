c741d399ffe231e679602de6ad5ca79d33cfd246
Anibal Bastias Soto <anibal.bastias@gmail.com>
2022-04-28 23:04:52 -0400

    Add Search View for the top app bar

---
+++ src/main/java/com/anibalbastias/uikitcompose/components/molecules/SearchTopBar.kt
@@ -0,0 +1,146 @@
+package com.anibalbastias.uikitcompose.components.molecules
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.runtime.*
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import com.anibalbastias.uikitcompose.components.atom.Body1
+
+@ExperimentalComposeUiApi
+@ExperimentalAnimationApi
+@Composable
+fun SearchTopBar(
+    backgroundColor: Color,
+    contentColor: Color,
+    searchText: String,
+    placeholderText: String = "",
+    onSearchTextChanged: (String) -> Unit = {},
+    onClearClick: () -> Unit = {},
+    onNavigateBack: () -> Unit = {},
+    matchesFound: Boolean,
+    results: @Composable () -> Unit = {},
+    noResults: @Composable () -> Unit = {},
+) {
+    Box {
+        Column(
+            modifier = Modifier
+                .background(backgroundColor)
+                .fillMaxSize()
+        ) {
+            SearchBar(
+                backgroundColor = backgroundColor,
+                contentColor = contentColor,
+                searchText = searchText,
+                placeholderText = placeholderText,
+                onSearchTextChanged = onSearchTextChanged,
+                onClearClick = onClearClick,
+                onNavigateBack = onNavigateBack
+            )
+
+            if (matchesFound) {
+                results()
+            } else {
+                if (searchText.isNotEmpty()) {
+                    noResults()
+                }
+            }
+        }
+    }
+}
+
+@ExperimentalAnimationApi
+@ExperimentalComposeUiApi
+@Composable
+fun SearchBar(
+    backgroundColor: Color,
+    contentColor: Color,
+    searchText: String,
+    placeholderText: String = "",
+    onSearchTextChanged: (String) -> Unit = {},
+    onClearClick: () -> Unit = {},
+    onNavigateBack: () -> Unit = {},
+) {
+    var showClearButton by remember { mutableStateOf(false) }
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val focusRequester = remember { FocusRequester() }
+
+    TopAppBar(
+        backgroundColor = backgroundColor,
+        title = { Text("") },
+        navigationIcon = {
+            IconButton(onClick = { onNavigateBack() }) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    tint = contentColor,
+                    modifier = Modifier,
+                    contentDescription = "Back"
+                )
+            }
+        },
+        actions = {
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 2.dp)
+                    .onFocusChanged { focusState ->
+                        showClearButton = (focusState.isFocused)
+                    }
+                    .focusRequester(focusRequester),
+                value = searchText,
+                onValueChange = onSearchTextChanged,
+                placeholder = {
+                    Body1(text = placeholderText, color = contentColor.copy(alpha = 0.6f))
+                },
+                colors = TextFieldDefaults.textFieldColors(
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    backgroundColor = Color.Transparent,
+                    textColor = contentColor,
+                    cursorColor = LocalContentColor.current.copy(alpha = LocalContentAlpha.current)
+                ),
+                trailingIcon = {
+                    AnimatedVisibility(
+                        visible = showClearButton,
+                        enter = fadeIn(),
+                        exit = fadeOut()
+                    ) {
+                        IconButton(onClick = { onClearClick() }) {
+                            Icon(
+                                imageVector = Icons.Filled.Close,
+                                contentDescription = "Close",
+                                tint = contentColor
+                            )
+                        }
+                    }
+                },
+                maxLines = 1,
+                singleLine = true,
+                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(onDone = {
+                    keyboardController?.hide()
+                }),
+            )
+        })
+
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
+}
